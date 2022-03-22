fe0faeab012b2f5142b7ec25c28ee84265acc638
molottva <molottva@list.ru>
2022-03-22 13:50:46 +0300

    Added logic and input limit

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,7 +1,45 @@
 public class BMIService {
 
-    public float calculate(int weight, int height){
-        float bodyMassIndex = weight * 10_000 / ((float) height * (float) height); //10_000 это из-за перевода см в метры (100^2)
+    public String calculate(int weight, int height){
+        String bodyMassIndex;
+        if(weight <= 0 | weight > 250){
+            bodyMassIndex = "Неверный вес";
+        }
+        else if(height <= 50 | height > 250){
+            bodyMassIndex = "Неверный рост";
+        }
+        else{
+            double bodyMassIndexDouble = weight * 10_000 / Math.pow(height, 2); //10_000 из-за перевода см в метры (100^2)
+            bodyMassIndexDouble = (double) ((int) (bodyMassIndexDouble * 100)) / 100; // округление до двух чисел после запятой
+            bodyMassIndex = String.valueOf(bodyMassIndexDouble); //перевод из double в String
+            if(bodyMassIndexDouble <= 18.5){
+                if(bodyMassIndexDouble <= 16){
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас выраженный дефицит массы тела.";
+                }
+                else{
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас недостаточный (дефицит) массы тела";
+                }
+            }
+            else if(bodyMassIndexDouble > 18.5 & bodyMassIndexDouble <= 30){
+                if(bodyMassIndexDouble <= 25){
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". Ваш вес в норме.";
+                }
+                else{
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас избыточная масса тела (предожирение).";
+                }
+            }
+            else{
+                if(bodyMassIndexDouble <= 35){
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас ожирение 1 степени.";
+                }
+                else if(bodyMassIndexDouble <= 40){
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас ожирение 2 степени.";
+                }
+                else{
+                    bodyMassIndex = "Ваш Индекс Массы Тела равен " + bodyMassIndex + ". У вас ожирение 3 степени.";
+                }
+            }
+        }
 
         return bodyMassIndex;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,42 @@ public class Main {
         int weight = 77; //масса в кг
         int height = 170; //рост в см (так интереснее, чем сразу в метрах считать)
 
-        float bodyMassIndex = service.calculate(weight, height);
-        System.out.println("Ваш индекс массы тела равен " + bodyMassIndex);
+        String bodyMassIndex;
+
+        bodyMassIndex = service.calculate(weight, height);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(0, 180);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(251, 180);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(80, 50);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate(80, 251);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 50, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 60, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 70, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 100, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 120, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 140, 190);
+        System.out.println(bodyMassIndex);
+
+        bodyMassIndex = service.calculate( 160, 190);
+        System.out.println(bodyMassIndex);
     }
 }
